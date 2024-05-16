26f8385f75f30c5b3597f6acccb1caffc59c1282
mumarhussain <mumarhussain126@gmail.com>
2024-05-17 01:37:03 +0500

    Complete the previous task and now start cloning Whatsapp

---
+++ src/constant/data.ts
@@ -0,0 +1,213 @@
+
+// export const arr = [
+//     {
+//         id:1,
+//         Color: "red",
+//     },
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     },
+//     {
+//         id:1,
+//         Color: "red",
+//     },
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     }
+// ]
+
+// export const arr2 = [
+//     {
+//         id:1,
+//         Color: "red",
+//     },
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     },
+//     {
+//         id:1,
+//         Color: "red",
+//     },
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     }
+//     ,
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     },
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     }
+//     ,
+//     {
+//         id:2,
+//         Color: "blue",
+//     },
+//     {
+//         id:3,
+//         Color: "green",
+//     },
+//     {
+//         id:4,
+//         Color: "yellow",
+//     },
+//     {
+//         id:5,
+//         Color: "purple",
+//     }
+// ]
+
+export const dataArr = [
+    {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    },
+    {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    },
+    , {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, , {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, , {
+        id: "unread",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+    }, , {
+        id: "group",
+        img: 'https://www.facebook.com/photo/?fbid=884926963309037&set=a.104076944727380',
+        name: "M Umar Hussain",
+        text: "Kya haal ha brother"
+
+    },
+]
